ad36279a13b2bc6696dc9c774327ca735d8f9ead
Kematia <tommasobartolucci11@gmail.com>
2021-05-21 12:07:46 +0200

    add options interface

---
+++ app/src/interfaces/_custom/many-to-one-dropdown/options.vue
@@ -0,0 +1,65 @@
+<template>
+	<div>
+		<v-form v-model="_value" :fields="fields" />
+	</div>
+</template>
+<script lang="ts">
+import { computed, defineComponent } from '@vue/composition-api';
+
+export default defineComponent({
+	props: {
+		value: {
+			type: Object,
+			default: () => ({}),
+		},
+	},
+	setup(props, { emit }) {
+		const _value = computed({
+			get() {
+				return props.value;
+			},
+			set(newValue: any) {
+				emit('input', { ...(props.value || {}), ...(newValue || {}) });
+			},
+		});
+
+		const fields = [
+			{
+				name: 'Parent Field',
+				field: 'parentField',
+				type: 'string',
+				meta: {
+					interface: 'input',
+					width: 'half',
+					options: {
+						placeholder: 'Parent field',
+						dbSafe: true,
+						font: 'monospace',
+					},
+				},
+				schema: null,
+			},
+			{
+				name: 'Children Field',
+				field: 'childrenField',
+				type: 'string',
+				meta: {
+					interface: 'input',
+					width: 'half',
+					options: {
+						placeholder: 'Children field',
+						dbSafe: true,
+						font: 'monospace',
+					},
+				},
+				schema: null,
+			},
+		];
+
+		return {
+			_value,
+			fields,
+		};
+	},
+});
+</script>
